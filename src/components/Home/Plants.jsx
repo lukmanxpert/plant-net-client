dc3bab062c0329ae9b1c20f062618819e36af72c
lukmanxpert <lukmanmiah2004@gmail.com>
2025-03-09 01:59:58 +0600

    load all plants

--- src/components/Home/Plants.jsx
+++ src/components/Home/Plants.jsx
@@ -1,12 +1,26 @@
 import Card from './Card'
 import Container from '../Shared/Container'
-
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
 const Plants = () => {
+  const { data: plants } = useQuery({
+    queryKey: ["plants"],
+    queryFn: async () => {
+      const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/plants`)
+      return data;
+    }
+  })
   return (
     <Container>
-      <div className='pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
-        <Card />
-      </div>
+      {
+        plants ? <div className='pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
+          {
+            plants.map(plant => <Card key={plant._id} plant={plant} />)
+          }
+        </div> : <div className='min-h-screen flex justify-center items-center'>
+          <p className='text-xl font-bold'>No data available!</p>
+        </div>
+      }
     </Container>
   )
 }
